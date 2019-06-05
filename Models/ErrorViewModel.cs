f5ed5749a3dd5014dad7649e2a6ac91ff1747604
Matheus Amaral De Camargo Cardoso Lima <41813664803@info.local>
2019-06-05 11:27:35 -0300

    Quarta

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Full_Ponto_Digital.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Full_Ponto_Digital.Models
+{
+    public class Usuario
+    {
+        public string Nome {get;set;}
+        public string Email {get;set;}
+        public string Senha {get;set;}
+        public DateTime DataNascimento {get;set;}
+    }
+}
